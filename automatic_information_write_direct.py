e1fbe3077c6e9951eb1be3c747e20036e92b205d
Huwenqiang Xiaomi Manjaro <49776858+Hu-Wenqiang@users.noreply.github.com>
2021-12-13 16:13:45 +0800

    2021.12.13 Updated Code And Become More Moduled

--- automatic_information_write_direct.py
+++ automatic_information_write_direct.py
@@ -2,21 +2,17 @@ import xlrd
 import xlwt
 import time
 
-information_workbook = 'information.xls'
-input_workbook = 'input.xls'
-output_workbook = 'output.xls'
 current_time = time.asctime()
-input_workbook_col = 3
 
-
-def process_read (workbook_name, index_num, col_num):
+def process_read (workbook_name, workbook_index_num, workbook_col_num):
     raw = xlrd.open_workbook (workbook_name)
-    information_sheet = raw.sheet_by_index (index_num)
-    information_sheet_col = information_sheet.col_values (col_num)
+    information_sheet = raw.sheet_by_index (workbook_index_num)
+    information_sheet_col = information_sheet.col_values (workbook_col_num)
     return information_sheet_col
 
-# 提前读取出来，避免无谓的重复读取，可将CPU time 从 5s 降到 0.1s
-to_be_compared = process_read ('input.xls', 0, input_workbook_col)
+# 提前读取出来，避免无谓的重复读取，可将CPU time 从 5s 降到 0.1sok_index_num = 0
+def process_get_data (input_workbook, input_workbook_index_num, input_workbook_col):
+    return process_read (input_workbook, input_workbook_index_num, input_workbook_col)
 
 # 提前打开Workbook，避免多次创建文件，导致最后只被覆盖来只剩下数二的
 wk = xlwt.Workbook()
@@ -24,26 +20,33 @@ sheet = wk.add_sheet ("result", cell_overwrite_ok=False)
 
 # 定义Style（指表头说明）
 style_bold = xlwt.easyxf ('font: bold on')
-sheet.write_merge (0, 0, 0, 3, current_time)
+sheet.write_merge (0, 0, 0, 3, current_time) # 先把第一行写了
 
-def compare_data (workbook_name, index_num, col_num, input_workbook_name, input_index_num, input_col_num):
-    result = [temp for temp in process_read (workbook_name, index_num, col_num) if temp in str(to_be_compared).strip()]
+def compare_data (workbook_name, workbook_index_num, workbook_col_num, input_workbook_name, input_workbook_index_num, input_workbook_col):
+    to_be_compared = process_get_data (input_workbook_name, input_workbook_index_num, input_workbook_col)
+    result = [temp for temp in process_read (workbook_name, workbook_index_num, workbook_col_num) if temp in str(to_be_compared).strip()]
     return result
 
-def process_write (result_data, workbook_name, index_num, col_num, title_name):
-    sheet.write (1, col_num, title_name, style_bold)
-    #i = 1
+def process_write (result_data, workbook_name, workbook_index_num, workbook_col_num, title_name):
+    sheet.write (1, workbook_col_num, title_name, style_bold)
     i = 2 # 保留第一行来放时间
     for temp in result_data:
-        sheet.write (i, col_num, temp)
+        sheet.write (i, workbook_col_num, temp)
         i = i+1
     wk.save (workbook_name)
 
 def main():
-    result_shi_1 = compare_data (information_workbook, 0, 0, input_workbook, 0, input_workbook_col)
-    result_shi_2 = compare_data (information_workbook, 1, 0, input_workbook, 0, input_workbook_col)
-    result_shu_1 = compare_data (information_workbook, 2, 0, input_workbook, 0, input_workbook_col)
-    result_shu_2 = compare_data (information_workbook, 3, 0, input_workbook, 0, input_workbook_col)
+    information_workbook = 'information.xls'
+    input_workbook_name = 'input.xls'
+    output_workbook = 'output.xls'
+    
+    input_workbook_index_num = 0
+    input_workbook_col = 3
+
+    result_shi_1 = compare_data (information_workbook, 0, 0, input_workbook_name, input_workbook_index_num, input_workbook_col)
+    result_shi_2 = compare_data (information_workbook, 1, 0, input_workbook_name, input_workbook_index_num, input_workbook_col)
+    result_shu_1 = compare_data (information_workbook, 2, 0, input_workbook_name, input_workbook_index_num, input_workbook_col)
+    result_shu_2 = compare_data (information_workbook, 3, 0, input_workbook_name, input_workbook_index_num, input_workbook_col)
 
     process_write (result_shi_1, output_workbook, 0, 0, "视听一区")
     process_write (result_shi_2, output_workbook, 0, 1, "视听二区")
